caf0d64ac9717b04a2644266cd1172d3ba1bfe6e
Alexey Alexandrov <a.alexandrov@aerolabs.ru>
2020-05-14 12:44:38 +0300

    Исправил все замечания

--- script/popup.js
+++ script/popup.js
@@ -1,21 +1,23 @@
-let popupProfile = document.querySelector('.popup-profile');
-let popupAddCard = document.querySelector('.popup-addcard');
-let popupImageView = document.querySelector('.popup-image');
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupAddCard = document.querySelector('.popup_type_addcard');
+const popupImageView = document.querySelector('.popup_type_image');
 const edit = document.querySelector('.profile__edit-button');
 const addCardButton = document.querySelector('.profile__add-button');
 const popupProfileButton = popupProfile.querySelector('.popup__close');
 const popupAddCardButton = popupAddCard.querySelector('.popup__close');
 const popupImageButton = popupImageView.querySelector('.popup__close');
-let popupImageLink = popupImageView.querySelector('.popup__img');
-let popupImageCaption = popupImageView.querySelector('.popup__figcaption');
-let author = document.querySelector('.profile__title');
-let desc = document.querySelector('.profile__subtitle');
-let popupAuthor = document.querySelector('input[name="popupAuthor"]');
-let popupDesc = document.querySelector('input[name="popupDesc"]');
+const popupImageLink = popupImageView.querySelector('.popup__img');
+const popupImageCaption = popupImageView.querySelector('.popup__figcaption');
+const author = document.querySelector('.profile__title');
+const desc = document.querySelector('.profile__subtitle');
+const popupAuthor = document.querySelector('input[name="popupAuthor"]');
+const popupDesc = document.querySelector('input[name="popupDesc"]');
 const cardName = document.querySelector('input[name="popupName"]');
 const cardLink = document.querySelector('input[name="popupLink"]');
 const formProfile = popupProfile.querySelector('.popup__container');
 const formAddCard = popupAddCard.querySelector('.popup__container');
+const cardTemplate = document.querySelector('#fotoCard').content;
+const cardList = document.querySelector('.elements__list');
 const initialCards = [
   {
       name: 'Архыз',
@@ -43,56 +45,51 @@ const initialCards = [
   }
 ];
 
-function popupOpen(popup) {
-  popup.classList.remove('popup_type_closed');
-  popup.classList.add('popup_type_opened');
+function popup(popup) {
+  popup.classList.toggle('popup_opened');
 }
 
-function popupClose(popup) {
-  popup.classList.remove('popup_type_opened');
-  popup.classList.add('popup_type_closed');
+function imageView(name, link) {
+  popupImageLink.src = link;
+  popupImageLink.alt = name;
+  popupImageCaption.textContent = name;
+  popup(popupImageView);
 }
 
-function addCard(name, link, position) {
-  const cardTemplate = document.querySelector('#fotoCard').content;
-  const cardList = document.querySelector('.elements__list');
+function deleteCard(evt) {
+  const rmButton = evt.target;
+  const listCard = rmButton.closest('.elements__item');
+  listCard.remove();
+}
+
+function like(evt) {
+  const likeButton = evt.target;
+  likeButton.classList.toggle('elements__like-button_active');
+}
+
+function addCard(name, link) {
   const cardItem = cardTemplate.cloneNode(true);
 
   cardItem.querySelector('.elements__image').src = link;
   cardItem.querySelector('.elements__image').alt = name;
   cardItem.querySelector('.elements__text').textContent = name;
+  cardItem.querySelector('.elements__image').addEventListener('click', function(){ imageView(name, link) });
+  cardItem.querySelector('.elements__delete-button').addEventListener('click', deleteCard);
+  cardItem.querySelector('.elements__like-button').addEventListener('click', like);
 
-  cardItem.querySelector('.elements__image').addEventListener('click', function(){
-    popupImageLink.src = link;
-    popupImageLink.alt = name;
-    popupImageCaption.textContent = name;
-    popupOpen(popupImageView);
-  });
-
-  cardItem.querySelector('.elements__delete-button').addEventListener('click', function(evt){
-    const rmButton = evt.target;
-    const listCard = rmButton.closest('.elements__item');
-    listCard.remove();
-  });
-
-  cardItem.querySelector('.elements__like-button').addEventListener('click', function(evt){
-    let likeButton = evt.target;
-    likeButton.classList.toggle('elements__like-button_active');
-  });
-
-  position === 'prepend' ? cardList.prepend(cardItem) : cardList.append(cardItem);
+  return cardItem;
 }
 
 function renderCards(cards) {
   cards.forEach(item => {
-    addCard(item.name, item.link, 'append');
+    cardList.append(addCard(item.name, item.link));
   });
 }
 
 function authorEdit() {
   popupAuthor.value = author.textContent;
   popupDesc.value = desc.textContent;
-  popupOpen(popupProfile);
+  popup(popupProfile);
 }
 
 function formSubmitProfile (evt) {
@@ -101,22 +98,22 @@ function formSubmitProfile (evt) {
   author.textContent = popupAuthor.value;
   desc.textContent = popupDesc.value;
 
-  popupClose(popupProfile);
+  popup(popupProfile);
 }
 
 function formSubmitAddCard (evt) {
   evt.preventDefault();
 
-  addCard(cardName.value, cardLink.value, 'prepend');
+  cardList.prepend(addCard(cardName.value, cardLink.value));
 
-  popupClose(popupAddCard);
+  popup(popupAddCard);
 }
 
 renderCards(initialCards);
 formProfile.addEventListener('submit', formSubmitProfile);
 formAddCard.addEventListener('submit', formSubmitAddCard);
 edit.addEventListener('click', authorEdit);
-popupProfileButton.addEventListener('click', function() { popupClose(popupProfile) });
-addCardButton.addEventListener('click', function () { popupOpen(popupAddCard) });
-popupAddCardButton.addEventListener('click', function() { popupClose(popupAddCard) });
-popupImageButton.addEventListener('click', function() { popupClose(popupImageView) });
+popupProfileButton.addEventListener('click', () => { popup(popupProfile) });
+addCardButton.addEventListener('click', () => { popup(popupAddCard) });
+popupAddCardButton.addEventListener('click', () => { popup(popupAddCard) });
+popupImageButton.addEventListener('click', () => { popup(popupImageView) });
